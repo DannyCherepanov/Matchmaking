16d1b5947887767e62eeec6616aea16bbbe4cca0
066956699 <066956699@109R030573XJQ22.EDU.YRDSB.NET>
2018-01-15 12:05:10 -0500

    user pkg and new user types

---
+++ MatchMaking/src/usersPKG/User.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usersPKG;
+
+/**
+ *
+ * @author 341033801
+ */
+public class User implements Comparable{
+    
+    //private variables for user's data
+    
+    private String un;
+    //takes in encrypted password (ENCRYPTION DONE ELSEWHERE)
+    private String pw;
+
+    
+    @Override
+    public int compareTo(Object o) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+    
+}
---
+++ MatchMaking/src/usersPKG/student.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usersPKG;
+
+/**
+ *
+ * @author 066956699
+ */
+public class student extends User{
+    
+    //for determining matches, 0 for unspecified/other, 1 for male, 2 for female
+    private int gender;
+    
+    //true if looking for match, false if just looking for friend
+    private boolean romantic;
+    
+}
